1374adc9a1723eed2d808d590b90a8ee07c1eed2
Diana Enriquez <diana@incaztech.com>
2013-05-08 22:29:59 +0000

    Add a full_title helper

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '10e7f8992e2cd89c495d42cc12855d8fcfdc5806ec875c7ccaf0a97a039124f82c42e525fd7808862cd18026780d66bb4e9973b3cdd5b16f3cb32c3f376baaf4'
