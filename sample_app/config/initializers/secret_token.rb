f817ba10a57fb5a48b5252156f1b0704f51bf188
Diana Enriquez <diana@incaztech.com>
2013-05-10 21:16:47 +0000

    Finish user sigup

--- sample_app/config/initializers/secret_token.rb
+++ sample_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '10e7f8992e2cd89c495d42cc12855d8fcfdc5806ec875c7ccaf0a97a039124f82c42e525fd7808862cd18026780d66bb4e9973b3cdd5b16f3cb32c3f376baaf4'
+SampleApp::Application.config.secret_token = '30df044d52659429e4a8b0b0620397c49ff7acc241b9c8fc24aaad9f0292a3db3cd0ba77084213e74286ea4b9c5df3992cf16532c3e454a152e08889a8eb3d2e'
